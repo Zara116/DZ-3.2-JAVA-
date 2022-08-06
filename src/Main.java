976aa3f2336a4aae1bbb62c353a5294480f4edd4
Zara116 <zarema.mindubaeva@yandex.ru>
2022-08-06 16:31:19 +0300

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int m=74;
+        int h=172;
+        int bmi= service.calculate(m,h);
+                System.out.println(bmi);
+    }
+
 }
+
