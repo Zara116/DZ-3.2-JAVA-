1699e673999c58f98644453ef8900057470d8dd6
Zara116 <zarema.mindubaeva@yandex.ru>
2022-08-06 16:30:37 +0300

    src/BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class BmiService {
+public class BmiService {
+    public int calculate(double m, double h) {
+        double h2=(h/100);
+        double h3=h2*h2;
+        double bmi = (m/h3);
+
+
+
+        return (int) bmi;
+
+    }
+
+    {
+
+
+    }
 }
